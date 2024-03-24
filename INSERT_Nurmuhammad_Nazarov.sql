f2dcf1663c5d353a3913e86a3d556c2599a6857a
Nurmuhammad Nazarov <78847300+neonazarov@users.noreply.github.com>
2024-03-24 06:04:58 +0500

    Update INSERT_Nurmuhammad_Nazarov.sql

--- INSERT_Nurmuhammad_Nazarov.sql
+++ INSERT_Nurmuhammad_Nazarov.sql
@@ -1,27 +1,25 @@
+BEGIN TRANSACTION;
+
+-- Insert film
 INSERT INTO film (title, rental_rate, rental_duration)
-VALUES ('Forsage', 4.99, 14);
+VALUES ('Inception', 4.99, 2);
 
-
+-- Assuming 'Inception' gets a film_id of 1, adjust based on your DB's auto-increment value
+-- Insert actors
 INSERT INTO actor (first_name, last_name)
-VALUES ('Vin', 'Diesel'),
-       ('Paul', 'Walker'),
-       ('Michelle', 'Rodriguez');
+VALUES ('Leonardo', 'DiCaprio'),
+       ('Joseph', 'Gordon-Levitt'),
+       ('Ellen', 'Page');
 
+-- Insert film_actor links
+-- Adjust actor_ids based on the auto-increment values assigned to the new actors
+INSERT INTO film_actor (actor_id, film_id)
+VALUES ((SELECT actor_id FROM actor WHERE first_name = 'Leonardo' AND last_name = 'DiCaprio'), (SELECT film_id FROM film WHERE title = 'Inception')),
+       ((SELECT actor_id FROM actor WHERE first_name = 'Joseph' AND last_name = 'Gordon-Levitt'), (SELECT film_id FROM film WHERE title = 'Inception')),
+       ((SELECT actor_id FROM actor WHERE first_name = 'Ellen' AND last_name = 'Page'), (SELECT film_id FROM film WHERE title = 'Inception'));
 
-SELECT actor_id
-FROM actor
-WHERE first_name IN ('Vin', 'Paul', 'Michelle')
-  AND last_name IN ('Diesel', 'Walker', 'Rodriguez');
-
-
-INSERT INTO film_actor (film_id, actor_id)
-VALUES
-  (1, 1), 
-  (1, 2),
-  (1, 3); 
-
-
+-- Add movie to store's inventory, assuming store_id of 1
 INSERT INTO inventory (film_id, store_id)
-SELECT film_id, Z
-FROM film
-WHERE title = 'Forsage';
+VALUES ((SELECT film_id FROM film WHERE title = 'Inception'), 1);
+
+COMMIT;
